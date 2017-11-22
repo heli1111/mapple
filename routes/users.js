fdfe98c312bc4284b5459d3af9892d8c86810309
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-22 22:56:14 +0000

    new routes: maps, pins plus user.js basic routes added

--- routes/users.js
+++ routes/users.js
@@ -8,11 +8,30 @@ module.exports = (knex) => {
   router.get("/", (req, res) => {
     knex
       .select("*")
-      .from("users")
+      .from("user")
       .then((results) => {
         res.json(results);
     });
-  });
+  }),
+
+  router.post("/", (req, res) => {
+    knex
+      .insert([])
+      .into("user")
+
+  }),
+
+  router.put("/", (req, res) => {
+    knex("user")
+      .where() //.where('published_date', '<', 2000)
+      .update()
+  }),
+
+  router.delete("/", (req, res) => {
+    knex("user")
+      .where()
+      .del()
+  })
 
   return router;
 }
