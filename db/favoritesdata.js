c619906f5766f8fb78fe9c3a0825be02e730529f
Jessica <hepu24@gmail.com>
2017-11-23 19:14:22 +0000

    renamed attributes

--- db/favoritesdata.js
+++ db/favoritesdata.js
@@ -1,39 +1,39 @@
 // User data in JSON objects
 module.exports = [
     {
-        user_id: 1,
-        map_id: 1
+        fav_user_id: 1,
+        fav_map_id: 1
     },
     {
-        user_id: 1,
-        map_id: 2
+        fav_user_id: 1,
+        fav_map_id: 2
     },
     {
-        user_id: 2,
-        map_id: 1
+        fav_user_id: 2,
+        fav_map_id: 1
     },
     {
-        user_id: 2,
-        map_id: 2
+        fav_user_id: 2,
+        fav_map_id: 2
     },
     {
-        user_id: 3,
-        map_id: 3
+        fav_user_id: 3,
+        fav_map_id: 3
     },
     {
-        user_id: 3,
-        map_id: 4
+        fav_user_id: 3,
+        fav_map_id: 4
     },
     {
-        user_id: 4,
-        map_id: 1
+        fav_user_id: 4,
+        fav_map_id: 1
     },
     {
-        user_id: 4,
-        map_id: 2
+        fav_user_id: 4,
+        fav_map_id: 2
     },
     {
-        user_id: 4,
-        map_id: 3
+        fav_user_id: 4,
+        fav_map_id: 3
     },
 ]
--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -2,10 +2,10 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function (table) {
-    table.increments('id').primary();
-    table.string('username');
-    table.string('password');
-    table.string('email');
+    table.increments('user_id').primary();
+    table.string('user_username');
+    table.string('user_password');
+    table.string('user_email');
   });
 };
 
