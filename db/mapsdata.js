dd810505fe44ebcfde28455cf421d6ba899f55ab
Jessica <hepu24@gmail.com>
2017-11-23 18:14:56 +0000

    database update

--- db/mapsdata.js
+++ db/mapsdata.js
@@ -3,27 +3,32 @@ module.exports = [
     {
         name:'toronto',
         description: 'uo minus id, quod maxime placeat, facere possimu',
-        createdAt: new Date()
+        createdAt: new Date(),
+        user_id: 1
     },
     {
         name:'london',
         description: 'uo minus id, quod maxime placeat, facere possimu',
-        createdAt: new Date()
+        createdAt: new Date(),
+        user_id: 2
     },
     {
         name:'tokyo',
         description: 'uo minus id, quod maxime placeat, facere possimu',
-        createdAt: new Date()
+        createdAt: new Date(),
+        user_id: 3
     },
     {
         name:'nyc',
         description: 'uo minus id, quod maxime placeat, facere possimu',
-        createdAt: new Date()
+        createdAt: new Date(),
+        user_id: 2
     },
     {
         name:'beijing',
         description: 'uo minus id, quod maxime placeat, facere possimu',
-        createdAt: new Date()
+        createdAt: new Date(),
+        user_id: 4
     }
 ]
 
--- db/pinsdata.js
+++ db/pinsdata.js
@@ -4,37 +4,49 @@ module.exports = [
         name:'food',
         description:'Sed ut perspiciatis, unde omnis iste natus error sit',
         image:'http://i0.kym-cdn.com/photos/images/newsfeed/001/127/449/e75.jpeg',
-        createdAt: new Date()
+        createdAt: new Date(),
+        user_id: 2,
+        map_id: 3
     },
     {
         name:'drinks',
         description:'Sed ut perspiciatis, unde omnis iste natus error sit',
         image:'http://i0.kym-cdn.com/photos/images/newsfeed/001/127/449/e75.jpeg',
-        createdAt: new Date()
+        createdAt: new Date(),
+        user_id: 1,
+        map_id: 4
     },
     {
         name:'movies',
         description:'Sed ut perspiciatis, unde omnis iste natus error sit',
         image:'http://i0.kym-cdn.com/photos/images/newsfeed/001/127/449/e75.jpeg',
-        createdAt: new Date()
+        createdAt: new Date(),
+        user_id: 1,
+        map_id: 1
     },
     {
         name:'food2',
         description:'Sed ut perspiciatis, unde omnis iste natus error sit',
         image:'http://i0.kym-cdn.com/photos/images/newsfeed/001/127/449/e75.jpeg',
-        createdAt: new Date()
+        createdAt: new Date(),
+        user_id: 2,
+        map_id: 4
     },
     {
         name:'theater',
         description:'Sed ut perspiciatis, unde omnis iste natus error sit',
         image:'http://i0.kym-cdn.com/photos/images/newsfeed/001/127/449/e75.jpeg',
-        createdAt: new Date()
+        createdAt: new Date(),
+        user_id: 1,
+        map_id: 3
     },
     {
         name:'movies2',
         description:'Sed ut perspiciatis, unde omnis iste natus error sit',
         image:'http://i0.kym-cdn.com/photos/images/newsfeed/001/127/449/e75.jpeg',
-        createdAt: new Date()
+        createdAt: new Date(),
+        user_id: 3,
+        map_id: 2
     }
 ]
 
