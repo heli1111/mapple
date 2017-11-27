ce8536a7d5bfe1f282bcaf2cefd24e94890fba3b
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-27 16:06:10 +0000

    added new pins for presentation

--- db/pinsdata.js
+++ db/pinsdata.js
@@ -1,29 +1,29 @@
 // User data in JSON objects
 module.exports = [
     {
-        pin_name:'food',
-        pin_description:'Sed ut perspiciatis, unde omnis iste natus error sit',
-        pin_image:'http://i0.kym-cdn.com/photos/images/newsfeed/001/127/449/e75.jpeg',
+        pin_name:'Junk but good',
+        pin_description:'Pizza flavored burger? Burger flavored pizza? You name it',
+        pin_image:'http://thearenafitness.com/wp-content/uploads/2017/02/Overeat-Greasy-Foods.jpg',
         pin_createdAt: new Date(),
-        pin_latitude: 43.761017,
-        pin_longitude: -79.460270,
+        pin_latitude: 43.861017,
+        pin_longitude: -79.560270,
         pin_user_id: 2,
         pin_map_id: 3
     },
     {
-        pin_name:'drinks',
-        pin_description:'Sed ut perspiciatis, unde omnis iste natus error sit',
-        pin_image:'http://i0.kym-cdn.com/photos/images/newsfeed/001/127/449/e75.jpeg',
+        pin_name:'Bubbling pops',
+        pin_description:'Cheers, all!',
+        pin_image:'https://prod-dietdoctor-d3xpwb8eqyot4ma5.stackpathdns.com/wp-content/uploads/2017/04/Guide_Drinks_16x9b-400x225.jpg',
         pin_createdAt: new Date(),
-        pin_latitude: 43.878479,
-        pin_longitude: -79.323700,
+        pin_latitude: 43.866479,
+        pin_longitude: -79.390700,
         pin_user_id: 1,
         pin_map_id: 4
     },
     {
-        pin_name:'movies',
-        pin_description:'Sed ut perspiciatis, unde omnis iste natus error sit',
-        pin_image:'http://i0.kym-cdn.com/photos/images/newsfeed/001/127/449/e75.jpeg',
+        pin_name:'Popcorn Movies',
+        pin_description:'This is where I watched Jurassic Park',
+        pin_image:'http://www.familyfuncanada.com/vancouver/files/2016/07/Popcorn-with-soda-and-movies.jpg',
         pin_createdAt: new Date(),
         pin_latitude: 43.944983,
         pin_longitude: -79.545724,
@@ -31,9 +31,9 @@ module.exports = [
         pin_map_id: 1
     },
     {
-        pin_name:'food2',
-        pin_description:'Sed ut perspiciatis, unde omnis iste natus error sit',
-        pin_image:'http://i0.kym-cdn.com/photos/images/newsfeed/001/127/449/e75.jpeg',
+        pin_name:'Sugar and spice',
+        pin_description:'Taste of heaven',
+        pin_image:'https://www.tellwut.com/uploads/media/image/249327e1478545763o2298.jpg',
         pin_createdAt: new Date(),
         pin_latitude: 43.808479,
         pin_longitude: -79.793700,
@@ -41,13 +41,43 @@ module.exports = [
         pin_map_id: 4
     },
     {
-        pin_name:'theater',
-        pin_description:'Sed ut perspiciatis, unde omnis iste natus error sit',
-        pin_image:'http://i0.kym-cdn.com/photos/images/newsfeed/001/127/449/e75.jpeg',
+        pin_name:'Bloody Hell Theater',
+        pin_description:'Best comedies ever',
+        pin_image:'https://www.sonypictures.com/studios/images/rentalsandfacilities/theaterrentals/thumb_backstage.jpg',
         pin_createdAt: new Date(),
         pin_latitude: 43.708479,
         pin_longitude: -79.400700,
         pin_user_id: 1,
         pin_map_id: 3
+    },
+    {
+        pin_name:'Escape room',
+        pin_description:'What a fun!',
+        pin_image:'https://escaperoomindy.com/wp-content/uploads/2015/06/sherlock2.jpg',
+        pin_createdAt: new Date(),
+        pin_latitude: 43.808479,
+        pin_longitude: -79.793700,
+        pin_user_id: 2,
+        pin_map_id: 1
+    },
+    {
+        pin_name:'Chill park',
+        pin_description:'Nature calls',
+        pin_image:'http://www.surrey.ca/images/cos-master/pageImages/Hawthorne%20Park.JPG',
+        pin_createdAt: new Date(),
+        pin_latitude: 43.708479,
+        pin_longitude: -79.400700,
+        pin_user_id: 3,
+        pin_map_id: 1
+    },
+    {
+        pin_name:'Parade',
+        pin_description:'March of fun',
+        pin_image:'https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/630/354/75/wdpromedia.disney.go.com/media/wdpro-assets/dlr/parks-and-tickets/entertainment/disneyland/mickeys-soundsational-parade/mickeys-soundsational-parade-00.jpg?30042013073717',
+        pin_createdAt: new Date(),
+        pin_latitude: 43.744893,
+        pin_longitude: -79.385031,
+        pin_user_id: 3,
+        pin_map_id: 1
     }
 ]
